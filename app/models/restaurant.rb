daa6bf99edb5ee2ad3fb977ed2fdbf0fa22c7480
Fanny Argoud <argoud.fanny@gmail.com>
2020-03-06 17:01:06 +0000

    add photo to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :basket
-  has_many_attached :photos
+  has_one_attached :photo
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
