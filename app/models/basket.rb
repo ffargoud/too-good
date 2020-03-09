a5ea20c5a57cbf6c69d45e833aff320b81fb868d
Fanny Argoud <argoud.fanny@gmail.com>
2020-03-09 15:17:44 +0000

    Products page and Add a product page

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,5 +1,7 @@
 class Basket < ApplicationRecord
   belongs_to :restaurant
   has_many :basket_products
+  has_many :products, through: :basket_products
   has_many :order_baskets
+  has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :restaurants do
     resources :baskets
+    resources :products
   end
 
   get 'dashboard', to: 'restaurants#dashboard', as: 'dashboard'
