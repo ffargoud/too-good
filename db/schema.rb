19352e0789cff738bdce3c747e480d29353fc5a0
Drexel Kleber <drex95@gmail.com>
2020-03-10 10:34:01 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_122103) do
+ActiveRecord::Schema.define(version: 2020_03_09_142335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_122103) do
     t.datetime "updated_at", null: false
     t.string "role"
     t.boolean "admin", default: false
+    t.string "location", default: "Rua do Conde de Redondo 91B"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
